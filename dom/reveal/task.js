ae9fddeac7ee651e7f3ab43e515c9bc642a24f72
Test <dharzhevskij@gmail.com>
2025-11-30 10:22:00 +0300

    change task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -5,15 +5,12 @@ function checkReveal() {
                 const elementTop = element.getBoundingClientRect().top;
                 const windowHeight = window.innerHeight;
                 
-                // Элемент появляется, когда его верхняя граница находится в поле зрения
                 if (elementTop < windowHeight - 100) {
                     element.classList.add('reveal_active');
                 }
             });
         }
 
-        // Проверяем при загрузке страницы
         window.addEventListener('load', checkReveal);
         
-        // Проверяем при прокрутке
         window.addEventListener('scroll', checkReveal);
